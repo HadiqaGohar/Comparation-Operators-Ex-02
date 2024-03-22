0ac40fc854ea36be6a7dfaee15813cd5243c7ccb
HadiqaGohar <160311280+HadiqaGohar@users.noreply.github.com>
2024-03-22 08:17:15 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+//REL/COMPARATION OPERATOR
+var flower = "Rose";
+if (flower === "Tulip") {
+    console.log("I like Tulip");
+}
+if (flower === "Chambeli") {
+    console.log("I really like Chambeli flower smell");
+}
+if (flower === "Pink rose") {
+    console.log("I like Pink rose");
+}
+else
+    ;
+console.log("I like all flowers");
---
+++ main.ts
@@ -0,0 +1,11 @@
+//REL/COMPARATION OPERATOR
+//IF_ELSE CONDITION
+let flower = "Rose"
+if (flower === "Tulip") {
+    console.log ("I like Tulip")
+} if (flower === "Chambeli") {
+    console.log ("I really like Chambeli flower smell")
+} if (flower === "Pink rose") {
+    console.log ("I like Pink rose")
+} else ;
+    console.log ("I like all flowers")   
